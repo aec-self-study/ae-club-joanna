bb631a41e3f278e9e527ef8f94670a838c9edffc
mistavis <132738413+mistavis@users.noreply.github.com>
2023-05-31 13:29:32 +0000

    Create yml files and adjust column names in the models

--- coffee_shop_project/models/marts/customer_orders.sql
+++ coffee_shop_project/models/marts/customer_orders.sql
@@ -6,18 +6,18 @@ with customer_orders as (
     select
         customer_id,
         count(*) as number_of_orders,
-        min(created_at) as first_order_at
-    from {{ source('coffee_shop', 'orders') }} as orders
+        min(order_created_at) as first_order_at
+    from {{ ref('stg_orders') }} as orders
     group by 1
 )
 
 select
-    customers.id as customer_id,
-    customers.name,
-    customers.email,
+    customer_orders.customer_id,
+    customers.customer_name,
+    customers.customer_email,
     customer_orders.first_order_at,
     customer_orders.number_of_orders
 
-from {{ source('coffee_shop', 'customers') }} as customers
+from {{ ref('stg_customers') }} as customers
 left join customer_orders
-    on customers.id = customer_orders.customer_id
+    on customers.customer_id = customer_orders.customer_id
