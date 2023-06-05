3f67f676600080516843dbfb87d436e7cdbd1b99
mistavis <132738413+mistavis@users.noreply.github.com>
2023-06-05 16:13:20 +0000

    Add web tracking source and staging model

---
+++ coffee_shop_project/models/staging/web_tracking/stg_web_tracking__page_views.sql
@@ -0,0 +1,19 @@
+with source as (
+    select * from {{source('web_tracking', 'pageviews') }}
+),
+
+renamed as (
+    select
+        id as page_view_id,
+        visitor_id,
+        customer_id,
+
+        page as page_name,
+        device_type,
+
+        -- timestamps
+        timestamp as page_view_created_at
+    from source
+)
+
+select * from renamed
