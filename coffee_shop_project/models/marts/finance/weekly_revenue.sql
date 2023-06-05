83fd8c9dbd2a857c996412554eca987407a631ec
mistavis <132738413+mistavis@users.noreply.github.com>
2023-06-05 12:31:23 +0000

    Refactor and join models

--- coffee_shop_project/models/marts/finance/weekly_revenue.sql
+++ coffee_shop_project/models/marts/finance/weekly_revenue.sql
@@ -7,15 +7,16 @@ with order_details as (
 orders as (
     select
       order_id,
-      customer_id
+      customer_id,
+      order_created_at
     from {{ ref('stg_coffee_shop__orders') }}
 ),
 
 customers as (
     select
-      cutomer_id,
+      customer_id,
       first_order_at
-    from from {{ ref('int_customer_order_count') }}
+    from {{ ref('int_customer_order_count') }}
 ),
 
 combined as (
@@ -23,12 +24,12 @@ combined as (
       order_details.order_id,
       orders.customer_id,
       order_details.product_category,
-      case when order_details.order_created_at = customers.first_order_at then 'new' else 'recurring' end as customer_type,
+      case when orders.order_created_at = customers.first_order_at then 'new' else 'recurring' end as customer_type,
       orders.order_created_at,
       sum(price) as revenue
     from order_details
     left join orders
-      on order_details.order_id = order.order_id
+      on order_details.order_id = orders.order_id
     left join customers
       on orders.customer_id = customers.customer_id
     group by 1, 2, 3, 4, 5
@@ -39,11 +40,9 @@ final as (
       cast(date_trunc(order_created_at, week) as date) as date_week,
       product_category,
       customer_type,
-      sum(price) as revenue_usd
-    from order_details
+      sum(revenue) as revenue
+    from combined
     group by date_week, product_category, customer_type
 )
 
-select
-  *
-from final
+select * from final
