5d10d74bee268a24eba5bc57ef993ee581ac8077
mistavis <132738413+mistavis@users.noreply.github.com>
2023-06-05 11:42:43 +0000

    clean up mart finance folder

--- coffee_shop_project/models/marts/finance/monthly_new_customers.sql
+++ coffee_shop_project/models/marts/finance/monthly_new_customers.sql
@@ -6,6 +6,6 @@ select
     date_trunc(first_order_at, month) as order_month,
     count(*) as new_customers
  
-from {{ ref('customer_orders') }}
+from {{ ref('int_customer_order_count') }}
 
 group by 1
