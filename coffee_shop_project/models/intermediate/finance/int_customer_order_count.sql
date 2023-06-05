239aa3108403a94c4b45f07755bfd9c10168529a
mistavis <132738413+mistavis@users.noreply.github.com>
2023-06-05 11:32:52 +0000

    Rename files and clean up folder

--- coffee_shop_project/models/intermediate/finance/int_customer_order_count.sql
+++ coffee_shop_project/models/intermediate/finance/int_customer_order_count.sql
@@ -7,7 +7,7 @@ with customer_orders as (
         customer_id,
         count(*) as number_of_orders,
         min(order_created_at) as first_order_at
-    from {{ ref('stg_orders') }} as orders
+    from {{ ref('stg_coffee_shop__orders') }} as orders
     group by 1
 )
 
@@ -18,6 +18,6 @@ select
     customer_orders.first_order_at,
     customer_orders.number_of_orders
 
-from {{ ref('stg_customers') }} as customers
+from {{ ref('stg_coffee_shop__customers') }} as customers
 left join customer_orders
     on customers.customer_id = customer_orders.customer_id
