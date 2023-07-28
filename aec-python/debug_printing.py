6d6bed5eeebec69f43b43e1abe33b2e3a32ae73c
mistavis <132738413+mistavis@users.noreply.github.com>
2023-07-28 11:22:32 +0000

    Add debugging files

---
+++ aec-python/debug_printing.py
@@ -0,0 +1,10 @@
+import random
+
+denoms = list(range(10))
+random.shuffle(denoms)
+
+for i in range(10):
+    print(f'i: {i}')
+    denom = denoms[i]
+    print(f'denom: {denom}')
+    result = 100 / denom
